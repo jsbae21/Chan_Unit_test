85701d1cfa00cc3a104a3734ac549e97ba7efa7e
jsbae21 <baelong21@naver.com>
2018-07-20 15:58:12 +0900

    review

--- src/contents/review_003.java
+++ src/contents/review_003.java
@@ -11,19 +11,25 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * 
- * Date : 2018-07-13
+ * Date : 2018-07-12
  * Subject : review
- * Name : review_003
- * Scenario : 1.로그인
- * 			  2.무나물 검색
- * 			  3.상품상세 진입 > 내가 작성 한 후기 수정페이지 진입 (후기에 사진 등록)
- * 			  4.사진 삭제 후 수정 완료 버튼 Tap
- * 			  5.pop-up check
- *              (포토후기는 1장 이상의 사진을 첨부해야 됩니다. 사진 첨부 후 다시 시도해 주세요.)
+ * Name : TC_1
+ * Scenario : 1.무나물 구매이력 O
+ *            2.search > 무나물
+ *            3.후기작성
+ *            4.validation 후 등록
  * Assertion :  Login success
 				Search success
 				Open a review page success
-				pop-up check success
+				별점 TC_1 Pass
+				별점 text 체크 success
+				맛 TC_1 Pass
+				양 TC_1 Pass
+				가격 TC_1 Pass
+				평가 text 체크 success
+				내용 TC_1 Pass
+				내용 TC_2 Pass
+				review validation success
  *
  */
 
@@ -31,6 +37,7 @@ public class review_003 {
   private WebDriver driver;
   private StringBuffer verificationErrors = new StringBuffer();
  
+   
   @Before
   public void setUp() throws Exception {
     driver = new ChromeDriver();
@@ -42,6 +49,7 @@ public class review_003 {
 	public void p_001() throws Exception {
 		driver.get("https://alpha-www.baeminfresh.com/");
 		driver.findElement(By.xpath("//*[@id=\"lnb\"]/ul/li[1]/a")).click();
+		
 		driver.findElement(By.xpath("//*[@id=\"member_id\"]")).sendKeys("baelong7@naver.com"); //아이디 입력 
 		driver.findElement(By.xpath("//*[@id=\"pwd\"]")).sendKeys("q1w2e3r4t5"); //패스워드 입력  
 		driver.findElement(By.xpath("//*[@id=\"login\"]/div[1]/form/fieldset/button")).click(); //로그인 버튼 클릭 
@@ -55,30 +63,96 @@ public class review_003 {
 		Thread.sleep(1500);
 		System.out.println("Search success");
 		
-		//리뷰 페이지 진입  **condition : 1개의 사진 추가된 상태**
+		//리뷰 페이지 진입
 		driver.findElement(By.xpath("//*[@id=\"product_detail_bar\"]/div/ul/li[2]/a")).click(); //후기탭
-		Thread.sleep(1500); 
-		driver.findElement(By.xpath("//*[@id=\"detail_opinion\"]/div/div/div/div/div[3]/div/section[1]/div[1]/div[2]/ul/li[1]/a")).click();
+		Thread.sleep(1500);
+		driver.findElement(By.xpath("//*[@id=\"detail_opinion\"]/div/div/div/div/div[2]/div[2]/span/a")).click(); // 상품 후기 작성 click
 		Thread.sleep(2000);
 		System.out.println("Open a review page success");
 		
-		// Delete a photo
-		driver.findElement(By.xpath("//*[@id=\"foobar\"]/div/div/div[4]/div/div/div/div[1]/div/a")).click();
-		System.out.println("Delete photos success");
+		// 별점 Text Check
 		driver.findElement(By.xpath("//*[@id=\"submitBtn\"]")).click();
+		if ("별점을 선택해 주세요".equals(driver.findElement(By.xpath("//*[@id=\"foobar\"]/div/div/div[1]/div[1]/div")).getText())) {
+			System.out.println("별점 TC_1 Pass");
+			assertTrue(true);
+		} else {
+			System.out.println("별점 TC_1 Fail");
+			assertTrue(false);
+			}
+		System.out.println("별점 text 체크 success");
+		driver.findElement(By.xpath("//*[@id=\"foobar\"]/div/div/div[1]/div/span[1]")).click();
+		driver.findElement(By.xpath("//*[@id=\"foobar\"]/div/div/div[1]/div/span[3]")).click();
+		driver.findElement(By.xpath("//*[@id=\"foobar\"]/div/div/div[1]/div/span[5]")).click();
+		driver.findElement(By.xpath("//*[@id=\"foobar\"]/div/div/div[1]/div/span[7]")).click();
+		driver.findElement(By.xpath("//*[@id=\"foobar\"]/div/div/div[1]/div/span[9]")).click();
+		Thread.sleep(1500);
 		
-		// Switching to Alert        
-        Alert alert = driver.switchTo().alert();		
-        		
-        // Capturing alert message.    
-        String alertMessage= driver.switchTo().alert().getText();		  		
-        System.out.println(alertMessage); // Displaying alert message	 
-        Thread.sleep(1000);
-        		
-        // Accepting alert		
-        alert.accept();	
-        Thread.sleep(2000);
-		System.out.println("pop-up check success");
+		//평가 Text Check
+		driver.findElement(By.xpath("//*[@id=\"submitBtn\"]")).click();
+		if ("상품을 평가해 주세요".equals(driver.findElement(By.xpath("//*[@id=\"foobar\"]/div/div/div[2]/div/div")).getText())) {
+			System.out.println("맛 TC_1 Pass");
+			assertTrue(true);
+		} else {
+			System.out.println("맛 TC_1 Fail");
+			assertTrue(false);
+			}
+		driver.findElement(By.xpath("//*[@id=\"foobar\"]/div/div/div[2]/dl/div[1]/dd[1]/label")).click(); //별로에요
+		driver.findElement(By.xpath("//*[@id=\"foobar\"]/div/div/div[2]/dl/div[1]/dd[2]/label")).click(); //괜찮아요
+		driver.findElement(By.xpath("//*[@id=\"foobar\"]/div/div/div[2]/dl/div[1]/dd[3]/label")).click(); //괜찮아요
+		Thread.sleep(1500);
+		driver.findElement(By.xpath("//*[@id=\"submitBtn\"]")).click();
+		if ("상품을 평가해 주세요".equals(driver.findElement(By.xpath("//*[@id=\"foobar\"]/div/div/div[2]/div/div")).getText())) {
+			System.out.println("양 TC_1 Pass");
+			assertTrue(true);
+		} else {
+			System.out.println("양 TC_1 Fail");
+			assertTrue(false);
+			}
+		driver.findElement(By.xpath("//*[@id=\"foobar\"]/div/div/div[2]/dl/div[2]/dd[1]/label")).click(); //부족해요
+		driver.findElement(By.xpath("//*[@id=\"foobar\"]/div/div/div[2]/dl/div[2]/dd[2]/label")).click(); //적당해요
+		driver.findElement(By.xpath("//*[@id=\"foobar\"]/div/div/div[2]/dl/div[2]/dd[3]/label")).click(); //넉넉해요
+		Thread.sleep(1500);
+		driver.findElement(By.xpath("//*[@id=\"submitBtn\"]")).click();
+		if ("상품을 평가해 주세요".equals(driver.findElement(By.xpath("//*[@id=\"foobar\"]/div/div/div[2]/div/div")).getText())) {
+			System.out.println("가격 TC_1 Pass");
+			assertTrue(true);
+		} else {
+			System.out.println("가격 TC_1 Fail");
+			assertTrue(false);
+			}
+		driver.findElement(By.xpath("//*[@id=\"foobar\"]/div/div/div[2]/dl/div[3]/dd[1]/label")).click(); //비싸요
+		driver.findElement(By.xpath("//*[@id=\"foobar\"]/div/div/div[2]/dl/div[3]/dd[2]/label")).click(); //적당해요
+		driver.findElement(By.xpath("//*[@id=\"foobar\"]/div/div/div[2]/dl/div[3]/dd[3]/label")).click(); //저렴해요
+		Thread.sleep(1500);
+		System.out.println("평가 text 체크 success");
+		
+		//내용 Text Check
+		Thread.sleep(1500);
+		driver.findElement(By.xpath("//*[@id=\"submitBtn\"]")).click();
+		if ("내용을 입력해 주세요".equals(driver.findElement(By.xpath("//*[@id=\"foobar\"]/div/div/div[3]/div[1]/div")).getText())) {
+			System.out.println("내용 TC_1 Pass");
+			assertTrue(true);
+		} else {
+			System.out.println("내용 TC_1 Fail");
+			assertTrue(false);
+			}
+		Thread.sleep(1500);
+		
+		driver.findElement(By.xpath("//*[@id=\"foobar\"]/div/div/div[3]/div[2]/div/textarea")).sendKeys("후기 자동화 입력"); //9글자
+		driver.findElement(By.xpath("//*[@id=\"submitBtn\"]")).click();
+		if ("10자 이상 입력해 주세요".equals(driver.findElement(By.xpath("//*[@id=\"foobar\"]/div/div/div[3]/div[1]/div")).getText())) {
+			System.out.println("내용 TC_2 Pass");
+			assertTrue(true);
+		} else {
+			System.out.println("내용 TC_2 Fail");
+			assertTrue(false);
+			}
+		Thread.sleep(1500);
+		
+		driver.findElement(By.xpath("//*[@id=\"foobar\"]/div/div/div[3]/div[2]/div/textarea")).sendKeys("입니다"); //12글자 완료
+		driver.findElement(By.xpath("//*[@id=\"submitBtn\"]")).click();
+		Thread.sleep(2000);
+		System.out.println("review validation success");
 }
 
   @After
